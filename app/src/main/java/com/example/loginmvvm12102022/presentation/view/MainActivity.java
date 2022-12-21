bb0ef488414805d620e71b2620113a7e503c5ae7
Phat <phatdroid94@gmail.com>
2022-12-21 19:06:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginmvvm12102022/presentation/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.loginmvvm12102022.presentation.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loginmvvm12102022.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
